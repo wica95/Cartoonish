59920a52df7af7916766893b47b2d6a1a8866f51
Luis Carlos sj <wica95@hotmail.com>
2022-10-30 11:32:43 -0600

    cartas
    cartas ya implementadas

--- web_comic_react/mi-primera-aplicacion/src/components/Carta.js
+++ web_comic_react/mi-primera-aplicacion/src/components/Carta.js
@@ -1,22 +1,72 @@
 import React from "react";
+import "../css/Carta.css";
 
 function Carta (){
    return (
 
 
 
-    <div className="contenedor-carta" >
-        <img className="imagen-carta"
-        src={require("../img/berserk.jpg")}
-        alt= "foto de Berserk" />
-        <div className="contenedor-carta">
-            <p  className="nombre-carta">  Berserker </p>
-            <p  className="texto-carta">El mejor seisen</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+    <div className="container" >
+        <div   className=" card">
+
+            <figure>
+            <img className="imagen-carta"
+            src={require("../img/berserk.jpg")}
+            alt= "foto de Berserk" />
+            </figure>
+            <div className="contenido">
+                <h1>Berserker</h1>
+                <p>Ver el mejor seinen de la historia</p>
+                <a  href="#">Leer mas</a>
+
+            </div>
 
         </div>
+         <div   className=" card">
+            
+            <figure>
+            <img className="imagen-carta"
+            src={require("../img/solo_lev.jpg")}
+            alt= "foto de Berserk" />
+            </figure>
+            <div className="contenido">
+                <h1>Solo leveling</h1>
+                <p>Para levelear</p>
+                <a  href="#">Leer mas</a>
 
+            </div>
 
+        </div>
+        <div   className=" card">
+            
+            <figure>
+            <img className="imagen-carta"
+            src={require("../img/Bleach.png")}
+            alt= "foto de Berserk" />
+            </figure>
+            <div className="contenido">
+                <h1>Bleach</h1>
+                <p>Ver el mejor shonen de la historia</p>
+                <a  href="#">Leer mas</a>
+
+            </div>
+
+        </div>
+        <div   className=" card">
+            
+            <figure>
+            <img className="imagen-carta"
+            src={require("../img/blu_L.jpg")}
+            alt= "foto de Berserk" />
+            </figure>
+            <div className="contenido">
+                <h1>Blue Lock</h1>
+                <p>Manga de fucho</p>
+                <a  href="#">Leer mas</a>
+
+            </div>
+
+        </div>
 
     </div>
 
